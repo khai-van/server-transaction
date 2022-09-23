40e5760c95fe7b5ea425dd1d651867aa9abb8479
khaivan <levankhai6729@gmail.com>
2022-09-23 23:10:44 +0700

    update

--- main_test.go
+++ main_test.go
@@ -15,8 +15,8 @@ import (
 )
 
 const (
-	numberRequest = 1000
-	mainAccount   = "nVrsWDeiTX"
+	numberRequest = 10000        // number of register user
+	mainAccount   = "nVrsWDeiTX" // default user with big balance
 )
 
 func init() {
@@ -42,7 +42,7 @@ func TestApp(t *testing.T) {
 	app := api.NewServer(serverConfig)
 
 	e := httptest.New(t, app.Application)
-
+	// get first balance
 	resp := e.GET("/detail").WithQuery("account", mainAccount).Expect()
 	resp.Status(httptest.StatusOK)
 	balance := uint64(resp.JSON().Object().Raw()["payload"].(map[string]interface{})["user"].(map[string]interface{})["balance"].(float64))
@@ -51,17 +51,17 @@ func TestApp(t *testing.T) {
 	for i := 0; i < numberRequest; i++ {
 		wg.Add(1)
 		account := RandStringRunes(10)
-		go func() {
-			e.GET("/register").WithQuery("account", account).Expect().Status(httptest.StatusOK)
-			e.GET("/detail").WithQuery("account", account).Expect().Status(httptest.StatusOK)
-
+		go func() { // send request in diffenrence goroutine no waitting
+			e.GET("/register").WithQuery("account", account).Expect().Status(httptest.StatusOK) // test register
+			e.GET("/detail").WithQuery("account", account).Expect().Status(httptest.StatusOK)   // test get detail
+			// test transfer main account to this account
 			e.GET("/transfer").
 				WithQuery("from", mainAccount).
 				WithQuery("to", account).
 				WithQuery("amount", 100).
 				Expect().
 				Status(httptest.StatusOK)
-
+			// test transfer this account back to main account
 			e.GET("/transfer").
 				WithQuery("from", account).
 				WithQuery("to", mainAccount).
@@ -75,11 +75,11 @@ func TestApp(t *testing.T) {
 	}
 
 	wg.Wait()
-
+	// get detail main account
 	newResp := e.GET("/detail").WithQuery("account", mainAccount).Expect()
 	newResp.Status(httptest.StatusOK)
 	newBalance := uint64(newResp.JSON().Object().Raw()["payload"].(map[string]interface{})["user"].(map[string]interface{})["balance"].(float64))
-
+	// recalculate main account and compare with in system db
 	if (balance - (numberRequest*100 - numberRequest*50)) != newBalance {
 		t.Error("Wrong expect Balance ", (balance - (numberRequest*100 - numberRequest*50)), newBalance)
 	}
